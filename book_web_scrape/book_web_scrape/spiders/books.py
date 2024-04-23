e863b0266874dc9c3a0bd4f9b6b765cc4daf3461
AL1 <alibinkashif007@gmail.com>
2024-04-23 16:42:54 +0500

    Update Spider class

--- book_web_scrape/book_web_scrape/spiders/books.py
+++ book_web_scrape/book_web_scrape/spiders/books.py
@@ -1,26 +1,55 @@
 import scrapy
+import pandas as pd
 
 
 class BooksSpider(scrapy.Spider):
     name = "books"
+    books = []
     def start_requests(self):
         
+        # Book Website home page link
         main_url = "https://books.toscrape.com/"
         
         yield scrapy.Request(url=main_url, callback=self.main_parse)
 
     def main_parse(self, response):
-        # print(response.url)
+        # Getting all the links in the side bar panel
         sidebar_pages_paths = response.css('div.side_categories>ul>li>ul>li>a::attr(href)').getall()
-        print(len(sidebar_pages_paths))
+
+        
+        # Crawl each web page one by one
         for path in sidebar_pages_paths:
             url = 'https://books.toscrape.com/'+path
+            
+            # Follow each page link and parse the response body the page_parse function
             yield response.follow(url=url, callback=self.page_parse)
 
     def page_parse(self, response):
         title = response.css('h1::text').get()
-        print(title)
-        cards = response.css('.product_pod')
-        # for card in cards:
-            
         # print(title)
+        cards = response.css('.product_pod')
+        # print('Books : ', len(cards))
+        
+        for card in cards:
+            book_title = card.css('h3>a::attr(title)').get()
+            
+            product_price = card.css('.product_price>p::text').get()
+            
+            star_rating = card.css('p::attr(class)').get()
+            
+            books_dict = {
+                'Title' : book_title,
+                'Price' : product_price,
+                'Rating' : star_rating.split()[1],
+                'Category' : title
+            }
+            
+            BooksSpider.books.append(books_dict)
+            
+            # img = card.css('.image_container img::attr(src)').get()
+            # path = 'books.csv'
+            # csv_file = open(path, 'a')
+            # csv_file.write(book_title.replace(",",":") + "," + product_price[1:] + "," + star_rating.split()[1] + "," + title+"\n")
+
+        data = pd.DataFrame(BooksSpider.books)
+        data.to_csv('books.csv')
